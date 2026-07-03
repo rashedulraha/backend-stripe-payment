3ea8dfe862eae3e07ed5d9d80e10f3f90c49b027
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-03 15:16:35 +0600

    prisma backend

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { authRoutes } from "./modules/auth/auth.routes";
+import { commentRoutes } from "./modules/comment/comment.route";
+import { postRoutes } from "./modules/post/post.route";
+import { userRoutes } from "./modules/user/user.route";
+
+
+
+const app : Application = express();
+
+app.use(cors({
+    origin : config.app_url,
+    credentials : true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+
+
+app.get("/",(req : Request, res : Response) => {
+    res.send("Hello, World!");
+});
+
+// app.post()
+
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
+
+export default app;
