2b70128d253fe5f914ca678b7deeaeb0a0a28891
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-04 09:55:13 +0600

    adding stripe secrect key

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,4 +12,6 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+  stripe_Product_Key: process.env.PRODUCT_ID!,
+  stripe_api_key: process.env.STRIPE_SECRET_KEY!,
 };
